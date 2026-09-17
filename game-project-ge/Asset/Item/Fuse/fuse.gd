413cbef4901286403b289a5ef588b0e33246e4a5
SomeDude <thanaphumi.t@kkumail.com>
2026-09-18 03:07:55 +0700

    รื้อ

--- game-project-ge/Asset/Item/Fuse/fuse.gd
+++
@@ -1,9 +0,0 @@
-extends "res://Asset/ClassItem.gd"
-
-class_name Fuse
-
-func getInteractive() -> String:
-	return "Pick Up Fuse"
-
-func get_item_name() -> String:
-	return "Fuse"
--- game-project-ge/Asset/System/GameClock/time_display.gd
+++
@@ -1,9 +0,0 @@
-extends Label
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	GameClock.time_changed.connect(_on_game_clock_time_changed)
-
-func _on_game_clock_time_changed(hour: int, minute: int) -> void:
-	text = "%d : %d" % [hour,minute]
